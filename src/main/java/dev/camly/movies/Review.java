0bd122a9154b5859f58af19d4d68a3904dc3fbba
becuyt <109753738+lycuyt@users.noreply.github.com>
2024-07-01 13:13:36 +0700

    commit

--- src/main/java/dev/camly/movies/Review.java
+++ src/main/java/dev/camly/movies/Review.java
@@ -7,6 +7,8 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
 @Data
 @Document(collection  = "reviews")
 @AllArgsConstructor
@@ -15,4 +17,12 @@ public class Review {
     @Id
     private ObjectId id;
     private String body;
+    private LocalDateTime created;
+    private LocalDateTime updated;
+//    public  Review(){}
+    public Review(String body, LocalDateTime created, LocalDateTime updated) {
+        this.body = body;
+        this.created = created;
+        this.updated = updated;
+    }
 }
---
+++ src/main/java/dev/camly/movies/ReviewServices.java
@@ -0,0 +1,26 @@
+package dev.camly.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ReviewServices {
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    public  Review createReview(String reviewBody, String imdbId)
+    {
+        Review review = reviewRepository.insert(new Review(reviewBody, LocalDateTime.now(), LocalDateTime.now()));
+        mongoTemplate.update(Movies.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+        return review;
+    }
+}
